04f50411290f9ddaaa8c977838401f5cf6d64262
Adrien Jalabert <35221770+ajalabert@users.noreply.github.com>
2020-03-19 16:46:38 +0100

    add gamemanager and first impl of ui button

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Button : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var gameManager = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<GameManager>();
+        gameManager.ZoomChangedEvent += OnZoomChanged;
+    }
+
+    private void OnZoomChanged(object sender, ZoomEventArgs zoomEvent)
+    {
+        Debug.Log($"zoom={zoomEvent.IsZoomed}");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private bool _isZoomed = false;
+
+    private event EventHandler<ZoomEventArgs> _zoomChangedEvent;
+
+    public event EventHandler<ZoomEventArgs> ZoomChangedEvent
+    {
+        add => _zoomChangedEvent += value;
+        remove => _zoomChangedEvent -= value;
+    }
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnButtonClicked()
+    {
+        _isZoomed = !_isZoomed;
+        _zoomChangedEvent?.Invoke(this, new ZoomEventArgs(_isZoomed));
+    }
+    
+}
+
+public class ZoomEventArgs : EventArgs
+{
+    public ZoomEventArgs(bool isZoomed)
+    {
+        IsZoomed = isZoomed;
+    }
+
+    public bool IsZoomed { get; }
+
+}
