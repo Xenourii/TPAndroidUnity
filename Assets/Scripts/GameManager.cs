d6d5c58912fa008d12d08d0e933fa8a132e89957
Thibault Buathier <thibault.buathier@gmail.com>
2020-03-19 16:25:53 +0100

    add rotation by swipe

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private bool _isZoomed = false;
+
+
+
+    public event EventHandler<bool> OnZoom;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+
+    void OnButtonClicked()
+    {
+        _isZoomed = !_isZoomed;
+        OnZoom?.Invoke(this, _isZoomed);
+    }
+
+}
---
+++ Assets/Scripts/SwipeRotation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeRotation : MonoBehaviour
+{
+    private Vector2 previousPosition;
+    [SerializeField] private float speed;
+    private void Update()
+    {
+        if (Input.touchCount == 1)
+        {
+            var touch = Input.touches[0];
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    // Stockage du point de départ
+                    previousPosition = touch.position;
+                    break;
+                case TouchPhase.Moved:
+                    // Stockage du point de fin
+                    var distance = previousPosition.x - touch.position.x;
+                    transform.Rotate(Vector3.up * distance * speed * Time.deltaTime);
+
+                    previousPosition = touch.position;
+                    break;
+            }
+        }
+    }
+}
